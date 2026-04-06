e2b51415865d6002f3f3099b07fceeac6e149ad6
abdmoh123 <abdhawisa@gmail.com>
2026-04-06 20:12:15 +0100

    feat(CH3/L1): add circleshape from boot.dev
    Was slightly modified to have type hints and also raise
    NotImplementedError instead of pass for the override methods.

---
+++ circleshape.py
@@ -0,0 +1,29 @@
+"""Circle shape base class for asteroids - provided by boot.dev."""
+
+import pygame
+from pygame.math import Vector2
+from pygame.surface import Surface
+
+
+class CircleShape(pygame.sprite.Sprite):
+    """Base class for game objects."""
+
+    def __init__(self, x: float, y: float, radius: float) -> None:
+        """Constructor for CircleShape."""
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)  # pyright: ignore[reportUnknownMemberType, reportUnknownArgumentType, reportAttributeAccessIssue]
+        else:
+            super().__init__()
+
+        self.position: Vector2 = Vector2(x, y)
+        self.velocity: Vector2 = Vector2(0, 0)
+        self.radius: float = radius
+
+    def draw(self, screen: Surface) -> None:
+        """Draws the object to the screen."""
+        raise NotImplementedError()
+
+    def update(self, dt: float) -> None:
+        """Updates the object."""
+        raise NotImplementedError()
