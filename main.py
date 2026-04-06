56daafe74dc9cd65ea668fe244e43b7c8e4024ae
abdmoh123 <abdhawisa@gmail.com>
2026-04-06 19:35:37 +0100

    feat(CH2/L3): add main pygame loop

--- main.py
+++ main.py
@@ -2,6 +2,38 @@
 
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+from pygame.surface import Surface
+
+
+def pygame_init() -> Surface:
+    """Initialises the pygame instance.
+
+    Returns:
+        Pygame screen surface
+    """
+    _ = pygame.init()
+    return pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+def pygame_loop(screen: Surface) -> None:
+    """Main pygame loop.
+
+    Args:
+        screen: Pygame screen surface
+    """
+    while True:
+        log_state()
+
+        for event in pygame.event.get():
+            match event.type:
+                case pygame.QUIT:
+                    return
+                case _:
+                    pass
+
+        _ = screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 
 def main() -> None:
@@ -9,6 +41,8 @@ def main() -> None:
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame_loop(pygame_init())
 
 
 if __name__ == "__main__":
